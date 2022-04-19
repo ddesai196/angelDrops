9acfcfcfc191fdb029429e090be1b5ee1c3c5328
Dhruval <ddesai196@yahoo.com>
2022-04-19 18:36:04 +0530

    projectsetup

---
+++ assets/js/main.js
@@ -0,0 +1,155 @@
+(function ($) {
+  "use strict";
+
+  $(window).on("load", function () {
+    /* preloader activate */
+    $(".preloader-activate").removeClass("preloader-active");
+  });
+
+  /* sticky menu */
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 200) {
+      $(".sticker").removeClass("stick");
+      $("body").removeClass("space-pt--120");
+    } else {
+      $(".sticker").addClass("stick");
+      $("body").addClass("space-pt--120");
+    }
+  });
+
+  /* offcanvas menu active */
+  $("#header-menu-trigger").on("click", function (e) {
+    e.stopPropagation();
+    $("#offcanvas-menu").toggleClass("active");
+    $(".body-wrapper").toggleClass("active-overlay");
+    $("body").toggleClass("overflow-hidden");
+  });
+
+  /* remove active class on click other parts of the body */
+  $("body").on("click", function () {
+    $("#offcanvas-menu").removeClass("active");
+    $(".body-wrapper").removeClass("active-overlay");
+    $("body").removeClass("overflow-hidden");
+  });
+
+  /* svg inject */
+
+  /* background image set */
+  var bgSelector = $(".bg-img");
+  bgSelector.each(function (index, elem) {
+    var element = $(elem),
+      bgSource = element.data("bg");
+    element.css("background-image", "url(" + bgSource + ")");
+  });
+
+
+
+  $(".field-wrapper .field-placeholder").on("click", function () {
+    $(this).closest(".field-wrapper").find("input").focus();
+    $(this).closest(".field-wrapper").find("textarea").focus();
+  });
+  $(".field-wrapper input,.field-wrapper textarea").on("keyup", function () {
+    var value = $.trim($(this).val());
+    if (value) {
+      $(this).closest(".field-wrapper").addClass("hasValue");
+    } else {
+      $(this).closest(".field-wrapper").removeClass("hasValue");
+    }
+  });
+
+
+
+  var readURL = function (input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('.profile-img').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+
+  $(".file-upload").on('change', function () {
+    readURL(this);
+  });
+
+  $(".upload-button").on('click', function () {
+    $(".file-upload").click();
+  });
+
+  $(".shere-button").each(function () {
+    $(this).on('click', function () {
+      $(this).parent("#share-box").toggleClass('act');
+    });
+  });
+
+
+
+  // document.getElementById("shere-btn").addEventListener("click", function () {
+  //     document.getElementById("share-box").classList.toggle("act");
+  //   });
+
+  /* cart plus minus */
+  function checkdevice() {
+    if ($(window).width() < 991) {
+      $('body').removeClass('hide-content');
+    } else {
+      $('body').addClass('hide-content');
+    }
+  }
+  checkdevice();
+  $(window).on('resize', function () {
+    checkdevice();
+  });
+  const swiper = new Swiper('.home-slider-ctm', {
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 6,
+    pagination: false,
+    navigation: false,
+    // autoplay: {
+    //   delay: 2000,
+    // },
+    scrollbar: false,
+    slideToClickedSlide: true,
+  });
+
+  const swiper2 = new Swiper('.home-slider-ctm', {
+    loop: true,
+    spaceBetween: 34,
+    slidesPerView: 2,
+    pagination: false,
+    navigation: false,
+    // autoplay: {
+    //   delay: 2000,
+    // },
+    scrollbar: false,
+    slideToClickedSlide: true,
+  });
+
+  var CartPlusMinus = $(".cart-plus-minus");
+  CartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
+  CartPlusMinus.append('<div class="inc qtybutton">+</div>');
+  $(".qtybutton").on("click", function () {
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+    if ($button.text() === "+") {
+      var newVal = parseFloat(oldValue) + 1;
+    } else {
+      // Don't allow decrementing below zero
+      if (oldValue > 1) {
+        var newVal = parseFloat(oldValue) - 1;
+      } else {
+        newVal = 1;
+      }
+    }
+    $button.parent().find("input").val(newVal);
+  });
+
+
+
+
+})(jQuery);
+
